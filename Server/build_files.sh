eb8954fe54b9c8fb14aa952f26475f6681741196
Eajjagiri_Rajasheker <Eajjagiri_Rajasheker@steris.com>
2023-12-24 23:29:19 +0530

    d

--- Server/build_files.sh
+++ Server/build_files.sh
@@ -1,7 +1,7 @@
 echo "Build bash start"
 
-pip install -r requirements.txt
-python manage.py collectstatic
+python3.9 -m pip install -r requirements.txt
+python3.9  manage.py collectstatic  --noinput --clear
 # python manage.py runserver
 
 echo "Build done using bash file"
